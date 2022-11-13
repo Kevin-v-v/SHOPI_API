2806b0f2b0723778fa9212b0cd9b0a3210d8dc93
Kevin Villalobos <kevin.villalobosvl@outlook.com>
2022-11-13 00:35:47 -0600

    Cambio de base de datos a la nube login arreglado

--- Login/src/index.js
+++ Login/src/index.js
@@ -6,7 +6,6 @@ const dotenv = require('dotenv');
 const path = require('path');
 const session = require('express-session');
 const mongoStore = require('connect-mongo');
-const bodyParser = require('body-parser');
 
 dotenv.config({
     path: path.resolve(__dirname, '../' + process.env.NODE_ENV + '.env')
@@ -20,7 +19,7 @@ app.use(session({
     cookie: {
         maxAge: 1800000
     },
-    store: mongoStore.create({ mongoUrl: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_LOCATION}` })
+    store: mongoStore.create({ mongoUrl: process.env.DB_CONNECTION_STRING })
 }));
 
 require('./config/db.config');
