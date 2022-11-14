fc5166566f318ec24adf875f629d9f31cca0d65d
Kevin Villalobos <kevin.villalobosvl@outlook.com>
2022-11-13 18:31:24 -0600

    login arreglado, cors añadido

--- Login/src/index.js
+++ Login/src/index.js
@@ -6,11 +6,19 @@ const dotenv = require('dotenv');
 const path = require('path');
 const session = require('express-session');
 const mongoStore = require('connect-mongo');
-const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 dotenv.config({
     path: path.resolve(__dirname, '../' + process.env.NODE_ENV + '.env')
 });
+
+var corsOptions = {
+    origin: '*'
+    //optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+
 
 app.use(session({
     secret: [process.env.SECRET_KEY_1, process.env.SECRET_KEY_2],
@@ -20,11 +28,11 @@ app.use(session({
     cookie: {
         maxAge: 1800000
     },
-    store: mongoStore.create({ mongoUrl: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_LOCATION}` })
+    store: mongoStore.create({ mongoUrl: "mongodb://database:27017/SHOPI" })
 }));
 
 require('./config/db.config');
-
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(loginRoutes);
 app.use(authRoutes);
