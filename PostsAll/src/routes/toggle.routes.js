955ac58e9a6bd43be412d64cd6e304f0c629ae0c
Kevin Villalobos <kevin.villalobosvl@outlook.com>
2022-12-05 17:22:39 -0600

    delete toggle verb

--- PostsAll/src/routes/toggle.routes.js
+++ PostsAll/src/routes/toggle.routes.js
@@ -2,7 +2,7 @@ const {Router, json} = require('express');
 const toggleController = require('../controllers/toggle.controller');
 let router = Router();
 
-router.put('/api/posts/toggle', json(), toggleController);
+router.delete('/api/posts/toggle', json(), toggleController);
 
 
 module.exports = router;
